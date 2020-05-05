a2c60b9220e1d463a6f5ffdc83f4bba4e025f746
elena <eperez045@gmail.com>
2020-05-05 11:58:40 +0200

    SQL tienda final

--- SQL tienda/tienda.sql
+++ SQL tienda/tienda.sql
@@ -14,7 +14,6 @@ SELECT nombre FROM fabricante;
 SELECT nombre FROM fabricante ORDER BY nombre DESC;
 SELECT nombre, precio FROM producto ORDER BY nombre, precio DESC;
 SELECT * FROM fabricante LIMIT 5;
-
 SELECT nombre, precio FROM producto ORDER BY precio LIMIT 1;
 SELECT nombre, precio FROM producto ORDER BY precio DESC LIMIT 1;
 SELECT codigo_fabricante, nombre FROM producto WHERE codigo_fabricante=2;
@@ -25,7 +24,6 @@ SELECT nombre, precio FROM producto WHERE precio>80 AND precio<300;
 SELECT nombre, precio FROM producto WHERE precio>60 AND precio<200;
 SELECT nombre, precio, codigo_fabricante FROM producto WHERE precio>200 AND codigo_fabricante=2;
 SELECT nombre, codigo_fabricante FROM producto WHERE codigo_fabricante=1 OR codigo_fabricante=3 OR codigo_fabricante=5;
-
 SELECT nombre, (precio*100) AS centimos FROM producto;
 SELECT nombre FROM fabricante WHERE nombre LIKE "s%";
 SELECT nombre FROM fabricante WHERE nombre LIKE "%w%";
@@ -50,7 +48,6 @@ SELECT producto.nombre, fabricante.nombre, producto.precio FROM producto INNER J
 SELECT fabricante.codigo, fabricante.nombre FROM producto INNER JOIN fabricante ON producto.codigo_fabricante=fabricante.codigo;
 SELECT * FROM producto RIGHT JOIN fabricante ON producto.codigo_fabricante=fabricante.codigo;
 SELECT * FROM producto RIGHT JOIN fabricante ON producto.codigo_fabricante=fabricante.codigo WHERE =NULL;
-
 SELECT COUNT(nombre) FROM producto;
 SELECT COUNT(nombre) FROM fabricante;
 SELECT COUNT(distinct codigo_fabricante) FROM producto;
@@ -73,4 +70,32 @@ SELECT COUNT(precio) FROM producto WHERE precio>=180;
 SELECT COUNT(distinct codigo_fabricante) FROM producto WHERE precio>=180;
 SELECT codigo_fabricante, AVG(precio) FROM producto GROUP BY codigo_fabricante;
 SELECT AVG(producto.precio), fabricante.nombre FROM producto INNER JOIN fabricante ON producto.codigo_fabricante=fabricante.codigo GROUP BY fabricante.nombre;
+SELECT AVG(producto.precio) AS media, fabricante.nombre FROM producto INNER JOIN fabricante ON producto.codigo_fabricante=fabricante.codigo GROUP BY fabricante.nombre HAVING media>180;
+SELECT COUNT(producto.codigo) AS cantidad, fabricante.nombre FROM producto INNER JOIN fabricante ON producto.codigo_fabricante=fabricante.codigo GROUP BY fabricante.nombre HAVING cantidad>1;
+SELECT COUNT(producto.codigo) AS cantidad, fabricante.nombre FROM producto INNER JOIN fabricante ON producto.codigo_fabricante=fabricante.codigo WHERE producto.precio>220 GROUP BY fabricante.nombre;
+
+SELECT SUM(producto.precio) AS suma, fabricante.nombre FROM producto INNER JOIN fabricante ON producto.codigo_fabricante=fabricante.codigo GROUP BY fabricante.nombre HAVING suma>1000;
+SELECT SUM(producto.precio) AS maximo, producto.nombre, fabricante.nombre FROM producto INNER JOIN fabricante ON producto.codigo_fabricante=fabricante.codigo GROUP BY fabricante.nombre;
+SELECT producto.nombre, fabricante.nombre FROM producto, fabricante WHERE fabricante.codigo = producto.codigo_fabricante AND (SELECT fabricante.nombre="Lenovo");
+SELECT producto.nombre, fabricante.nombre, producto.precio FROM producto, fabricante WHERE fabricante.codigo = producto.codigo_fabricante AND producto.precio > (SELECT MAX(producto.precio) WHERE fabricante.nombre ="Lenovo");
+SELECT producto.nombre, fabricante.nombre, MAX(producto.precio) FROM producto, fabricante WHERE fabricante.codigo = producto.codigo_fabricante AND (SELECT fabricante.nombre ="Lenovo");
+SELECT producto.nombre, fabricante.nombre, MIN(producto.precio) FROM producto, fabricante WHERE fabricante.codigo = producto.codigo_fabricante AND (SELECT fabricante.nombre ="Hewlett-Packard");
+SELECT producto.nombre, fabricante.nombre, producto.precio FROM producto, fabricante WHERE fabricante.codigo = producto.codigo_fabricante AND producto.precio >=(SELECT MAX(producto.precio) FROM producto WHERE producto.codigo_fabricante=2);
+SELECT producto.nombre, fabricante.nombre, producto.precio FROM producto, fabricante WHERE fabricante.codigo = producto.codigo_fabricante AND producto.precio >(SELECT AVG(producto.precio) FROM producto WHERE producto.codigo_fabricante=1) AND codigo_fabricante=1;
+SELECT producto.nombre, fabricante.nombre, producto.precio FROM producto, fabricante WHERE fabricante.codigo = producto.codigo_fabricante;
+SELECT nombre FROM fabricante WHERE codigo IN(SELECT producto.codigo_fabricante FROM producto, fabricante WHERE fabricante.codigo=codigo_fabricante);
+SELECT nombre FROM fabricante WHERE codigo NOT IN(SELECT producto.codigo_fabricante FROM producto, fabricante WHERE fabricante.codigo=codigo_fabricante);
+SELECT nombre FROM fabricante WHERE EXISTS(SELECT * FROM producto WHERE fabricante.codigo=codigo_fabricante);
+SELECT nombre FROM fabricante WHERE NOT EXISTS(SELECT * FROM producto WHERE fabricante.codigo=codigo_fabricante);
+SELECT producto.nombre, fabricante.nombre, MAX(producto.precio) FROM producto, fabricante WHERE fabricante.codigo = producto.codigo_fabricante GROUP BY fabricante.nombre;
+
+SELECT producto.nombre, fabricante.nombre, MAX(producto.precio) FROM producto, fabricante WHERE fabricante.codigo = producto.codigo_fabricante AND (SELECT fabricante.nombre ="Lenovo");
+
+
+
+
+
+
+
+
 
